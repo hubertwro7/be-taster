5bb657d8e8d17beca857522983ffd03ece704c7d
hubertwro7 <hubertwro7@gmail.com>
2024-01-08 23:01:53 +0100

    Serilog implementation and configuration

--- Taster.Api/Program.cs
+++ Taster.Api/Program.cs
@@ -1,10 +1,27 @@
+using Serilog;
+
 namespace Taster.Api
 {
     public class Program
     {
+        public static string APP_NAME = "Taster.Api";
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("Application", APP_NAME)
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.UseSerilog((context, services, configuration) => configuration
+                .Enrich.WithProperty("Application", APP_NAME)
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext());
 
             // Add services to the container.
 
