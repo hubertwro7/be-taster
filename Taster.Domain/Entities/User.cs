8f15fb873a76fb6acc45527f7b09d6afc3d55b8e
hubertwro7 <hubertwro7@gmail.com>
2024-01-10 00:07:19 +0100

    added domain entity User

---
+++ Taster.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Taster.Domain.Common;
+
+namespace Taster.Domain.Entities
+{
+    public class User : DomainEntity
+    {
+        public required string Email { get; set; }
+        public required string Username { get; set; }
+        public required string HashedPassword { get; set; }
+        public DateTimeOffset RegisterDate { get; set; }
+    }
+}
