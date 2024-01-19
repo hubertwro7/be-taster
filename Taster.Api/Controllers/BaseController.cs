e4472b0cae583fa1fcb7ddd9d11ee9657439763f
hubertwro7 <hubertwro7@gmail.com>
2024-01-19 22:49:00 +0100

    added base controller and controller for users with method for adding users

---
+++ Taster.Api/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Taster.Api.Controllers
+{
+    public abstract class BaseController : ControllerBase
+    {
+        protected readonly ILogger logger;
+        protected readonly IMediator mediator;
+
+        public BaseController(ILogger logger, IMediator mediator)
+        {
+            this.logger = logger;
+            this.mediator = mediator;
+        }
+    }
+}
---
+++ Taster.Api/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Taster.Application.Logic.User;
+
+namespace Taster.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : BaseController
+    {
+        public UsersController(ILogger logger, IMediator mediator) : base(logger, mediator)
+        {
+        }
+
+        [HttpPost("add")]
+        public async Task<ActionResult> CreateUser([FromBody] CreateUserCommand.Request model)
+        {
+            var createUserResult = await mediator.Send(model);
+            return Ok(createUserResult);
+        }
+    }
+}
